7d622fcdaac0cb5f5e73e130dbfc1b126fc76df6
Valt1-0 <lai2val1@gmail.com>
2023-04-27 17:57:36 +0200

    update port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const port = process.env.PORT || 3001;
+const port = process.env.EXPRESS_PORT || 3000;
 
 const express = require("express");
 const mongoose = require("mongoose");
